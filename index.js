a083714546cbeb130d2a23fef0f17e3b3d6971cc
DALLAS WALKER PETERSON <53N531@GMAIL.COM>
2017-10-20 16:29:43 -0400

    SC Engine Requirement + Artist/Track info + Art complete. N - set a tags

--- index.js
+++ index.js
@@ -1,61 +1,84 @@
-// audio.src = source
-// audio.volume = 0-1 ** Set incremental fade-in?
 ////REFERENCE////
-// var cTime = video.currentTime; --- audio.currentTime = 0 && checkAudio.currentTime = audio.currentTime
-// video.currentTime = 35; ---^^^^
-// var x = document.getElementById("myBtn").name;
 
-// var playPause = document.getElementById('playPause')
-// var stopBtn = document.getElementById('stopBtn')
-// var nextBtn = document.getElementById('nextBtn')
-// var src = document.querySelector('audio').src
-// var audio = document.getElementById('audio')
-// var windowClass = document.getElementById('window').classList
-// var openBtn = document.getElementById('openBtn')
+// DEFINE GLOBAL VARIABLES //
+var pauseIt     = document.getElementById('pauseBtn');
+var stopIt      = document.getElementById('stopBtn');
+var nextIt      = document.getElementById('nextBtn');
+var windowClass = document.getElementById('window').classList;
+var playIt      = document.getElementById('playBtn');
+var openBtn     = document.getElementById('openBtn');
+var innerImg    = document.getElementById('innerImg');
+var trackInfo   = document.getElementById('trackInfo');
 
-//// Play/Pause Button                               * === Complete
-// Playstate variable (PSV) = false default *
-// if PSV === false (play button event sets psv to true && plays)*
-// if PSV === true  (play button event sets psv to pause(declare current song position?) && pauses)*
-// if PSV === pause (play button event sets psv to true && resumes playback @ saved song position(set song pos. first?))*
-// const AudioCtrls = (function() {
-//   const publicCtrls = {};
-//   var psv = false;
-//
-//   function playPause() {
-//     if psv === false   (psv === true && audio.play);
-//     if psv === true    (psv === 'pause' && audio.pause && pp = audio.currentTime);
-//     if psv === 'pause' (psv === true && audio.currentTime = pp && audio.play);
-//   }
+(function(ENV) {
+  const client_id = ENV.client_id;
+  console.log('hello world');
+  SC.initialize({
+    client_id: client_id
+  });
+
+  SC.get('/tracks', {
+    q: 'vaporwave'
+  }).then(function(tracks) {
+    let x = 1;
+    let currentTrack = tracks[x];
+    var trackList = tracks;
+    innerImg.src = currentTrack.artwork_url;
+    trackInfo.innerHTML = currentTrack.permalink + "<br>" + currentTrack.title;
+    SC.stream('/tracks/' + currentTrack.id).then(function(player) {
+      playIt.addEventListener ('click', function(){
+        player.play();
+      })
+      pauseIt.addEventListener('click', () => player.pause());
+      stopIt.addEventListener('click', function(){
+        player.pause();
+        player.seek(0);
+      })
+        nextIt.addEventListener('click', function(){
+          // player.pause();
+          // player.seek(0);
+          x++;
+          currentTrack = tracks[x];
+          console.log(currentTrack.id)
+          SC.stream('/tracks/' + currentTrack.id).then(function(player) {
+            innerImg.src = currentTrack.artwork_url;
+            trackInfo.innerHTML = currentTrack.permalink + "<br>" + currentTrack.title;
+            player.play();
+            playIt.addEventListener ('click', () => player.play());
+            pauseIt.addEventListener('click', () => player.pause());
+            stopIt.addEventListener('click', function(){
+              player.pause();
+              player.seek(0);
+            })
+          })
+
+        });
 
 
+    })
+  });
+
+})(ENV)
 
 //// Stop button (X on window bar)
 // Hides Window/player *
-// Set PSV === false && stop playback && set song position to 0
-
   // function stop() {
   //   psv === false;
   //   audio.pause;
   //   audio.currentTime = 0;
   //   windowClass.add('hider');
   // }
-
 //// Next/Track 2 Button
-// set audio src to track2.mp3*
-// set playstate = true && plays*
 
-  // function next() {
-  //   src = 'track2.mp3';
-  //   audio.play;
-  //   psv === true;
-  // }
+
+
+
+
 
 //   function openPlayer() {
 //     openBtn.onclick = windowClass.remove('hider');
 //   }
 // })()
-
 ////BACKGROUND RANDOMIZER////
 // $(document).ready(function(){
 //     var rotato = ['bgs/1.gif','bgs/2.gif','bgs/3.gif','bgs/4.gif','bgs/5.gif','bgs/6.gif','bgs/7.gif','bgs/13.gif','bgs/8.gif','bgs/9.gif','bgs/10.gif','bgs/11.gif','bgs/12.gif'];
@@ -64,9 +87,7 @@
 //     $('body').addClass(classToAdd);
 //
 // });
-
 ////PLAYLIST GENERATOR////
-
 ////PLAYLIST OPERATION////
-
 ////SHUFFLE FUNCTION////
+//Set incremental fade-in?
